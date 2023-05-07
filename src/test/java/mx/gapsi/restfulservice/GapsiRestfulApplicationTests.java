1d199d908a9d56f7cc1edabca569307b54a65d7f
Yamel <yalvarez@soaint.com>
2023-05-07 17:33:24 -0500

    Create repository (First commit)

---
+++ src/test/java/mx/gapsi/restfulservice/GapsiRestfulApplicationTests.java
@@ -0,0 +1,13 @@
+package mx.gapsi.restfulservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GapsiRestfulApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
